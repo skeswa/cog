74defdd2d939a881e47f4b22e83cbacc90b638b8
Sandile Keswa <me@sandile.io>
2024-04-10 21:16:57 -0400

    Wrote initial doc comment for Cog

--- lib/src/cog.dart
+++ lib/src/cog.dart
@@ -15,6 +15,36 @@ part 'cog_like.dart';
 part 'cogtext.dart';
 part 'manual_cog.dart';
 
+/// [Cog] is an abstraction that imbues application state with composability,
+/// observability, and reliability.
+///
+/// Cogs are designed to organize evolving, related data such that operating on
+/// it is akin to turning "interlocking cogs in a machine": The connections
+/// between related data should be explicit and easily understandable. Applying
+/// mutations to this data should be both ergonomic and trustworthy.
+///
+/// There are exactly two kinds of [Cog]:
+/// - **Automatic**\
+///   Automatic Cogs, created with the default `Cog(...)` constructor, derive
+///   their value by composing the values of other Cogs and/or external state.
+///   In practice, most Cogs should be automatic.
+/// - **Manual**\
+///   Manual Cogs are assigned values imperatively with the `write(...)` method.
+///   These Cogs, created with the `Cog.man(...)` constructor, are intended to
+///   be roots of the state graph, and should be used sparingly.
+///
+/// Automatic and manual combine to form a reactive state graph robust enough
+/// for applications spanning the full spectrum of complexity.
+///
+/// Cogs either wrap a single value of type [ValueType], or many values of type
+/// [ValueType] indexed by a particular [Spin]. Cogs that specify a [Spin] act
+/// sort of like a hash map, allowing listeners and other Cogs to subscribe to
+/// specific variants of a value. For example, it could make sense for a [Cog]
+/// tracking the time to use `TimeZone` as its [Spin]. In this instance,
+/// [SpinType] would be `TimeZone`.
+///
+/// Cogs that specify a [Spin] require that `spin` is always specified when
+/// reading from, or writing to that [Cog].
 sealed class Cog<ValueType, SpinType> implements CogLike<ValueType, SpinType> {
   String? debugLabel;
 
