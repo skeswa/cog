cba1e36448cca2961396ca38175cbb4084e630b2
Sandile Keswa <me@sandile.io>
2024-05-14 15:49:48 -0400

    Spin docs

--- lib/src/spin.dart
+++ lib/src/spin.dart
@@ -1,4 +1,11 @@
+/// Helper class used purely to specify the [SpinType] of a Cog.
+///
+/// {@macro cog_like.spin}
 final class Spin<SpinType> {
+  /// Creates a new [Spin] that does nothing except specify [SpinType].
+  ///
+  /// This constructor throws if [SpinType] is left unspecified. In this case,
+  /// a `dynamic` [SpinType] is assumed to be an unspecified [SpinType].
   Spin() {
     assert(() {
       if (SpinType == dynamic) {
